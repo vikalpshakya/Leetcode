e80539c11a4d496c49467e13dfb9b89132be065c
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-03 18:40:29 +0530

    Added file

---
+++ C++/1.Two Sum.cpp
@@ -0,0 +1,19 @@
+// Time complexity = O(n^2) , Space complexity = O(1) 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>output;
+        for ( int i = 0 ; i < nums.size() ; i++){
+            for ( int j = i+1 ; j < nums.size() ; j++){
+                if(nums[i]+nums[j]==target){
+                    output.push_back(i);
+                    output.push_back(j);
+                }
+
+            }
+        }
+        return output;
+        
+    }
+};
