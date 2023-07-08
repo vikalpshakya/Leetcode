cbef67cc395322b77adbfdeeb11171bae8519eda
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-08 23:03:31 +0530

    Added file

---
+++ C++/206. Reverse Linked List.cpp
@@ -0,0 +1,20 @@
+// ----Recursive approach---------
+// Time complexity O(n) , space complexity O(n)
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* newhead = reverseList(head->next);
+        ListNode* temp = newhead;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = head ;
+        temp->next->next=NULL;
+
+        return newhead;
+    }
+};
