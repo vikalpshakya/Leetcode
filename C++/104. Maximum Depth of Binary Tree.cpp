6f1a9368f1ab1584b1a1c15b7c454075272edd78
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-18 17:15:23 +0530

    Added file

---
+++ C++/104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,27 @@
+//  Time complexity: O(N)
+//  Space complexity: O(N)
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if ( root == NULL) return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return 1+ max(leftDepth , rightDepth) ;
+
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
