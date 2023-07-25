8749ec36529a9408f748943835a6a500fd8ac899
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-07-26 00:45:43 +0530

    Added file

---
+++ C++/107. Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,24 @@
+// TIME COMPLEXITY O(n) , SPACE COMPLEXITY O(n)
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==nullptr) return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> vec;
+            while(size--){
+                TreeNode* newHead = q.front();
+                q.pop();
+                vec.push_back(newHead->val);
+                if(newHead->left) q.push(newHead->left);
+                if(newHead->right) q.push(newHead->right);
+            }
+            ans.push_back(vec);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
