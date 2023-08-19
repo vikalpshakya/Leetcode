e6d425896d90dac42b89896250e0b0e08ce70bec
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-08-19 22:31:17 +0530

    Added file

---
+++ C++/2824. Count Pairs Whose Sum is Less than Target.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPairs_help(vector<int>&nums , int target , int start){
+        if(start>=nums.size()) return 0;
+        int count = 0;
+        for(int i = start+1 ; i < nums.size() ; ++i){
+            if(nums[start]+nums[i] < target) count++;
+        }
+        return count + countPairs_help(nums,target,start+1);
+    }
+    int countPairs(vector<int>& nums, int target) {
+        return countPairs_help(nums,target,0);
+    }
+};
+// TC O(N) SC O(N)
