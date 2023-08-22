b191e1314fbc1803a7560dbfd66a16105f1e9e77
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-08-22 23:48:37 +0530

    Added file

--- C++/1.Two Sum.cpp
+++ C++/1.Two Sum.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>output;
-        for ( int i = 0 ; i < nums.size() ; i++){
+        for ( int i = 0 ; i < nums.size() ; i++){  
             for ( int j = i+1 ; j < nums.size() ; j++){
                 if(nums[i]+nums[j]==target){
                     output.push_back(i);
