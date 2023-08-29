829236933198b361efe5129abfdab6b33829ac59
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-08-30 03:31:09 +0530

    Added file

---
+++ C++/2483. Minimum Penalty for a Shop..cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+// -------------NAIVE APPROACH ----------------//
+
+// Time complexity: O(n^2) , Space complexity: O(1)
+
+    int bestClosingTime1(string customers) {
+        int n = customers.length();
+        int minP = INT_MAX;
+        int minH = INT_MAX;
+        for(int i = 0; i < n; i++){
+            int currP = 0;
+            int j = i;
+            
+            while(j < n){
+                if(customers[j] == 'Y')
+                    currP++;
+                j++;
+            }
+
+            j = i-1;
+            while(j >= 0){
+                if(customers[j] == 'N')
+                    currP++;
+                j--;
+            }
+            if(currP < minP){
+                minP = currP;
+                minH = i;
+            }
+        }
+            // edge case , when shop closes at nth hour
+            int NthP = count(customers.begin(), customers.end(), 'N');
+            if(NthP < minP){
+                minP = NthP;
+                minH = n;
+            }
+            return minH;
+    }
+};
+
