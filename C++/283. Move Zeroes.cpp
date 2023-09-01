56293dbebd189263c3f8e5165f1e546c257dbdb7
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-09-02 01:19:53 +0530

    Added file

---
+++ C++/283. Move Zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+// Time complexity: O(n) , space complexity O(1)
+
+    void moveZeroes(vector<int>& nums) {
+        int i=0,j=0;
+        while(i<nums.size() && j<nums.size()){
+            if(nums[j]!=0){
+                swap(nums[i++],nums[j++]);
+            }
+            else{
+                j++;
+            }
+        }
+    }
+};
