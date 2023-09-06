568b41ef5a9ef72949e6cd450a76d41777fc1fc9
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-09-07 00:01:47 +0530

    Added file

---
+++ C++/268. Missing Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+
+// Tc: O(n) , SC: O(1)
+
+    int missingNumber(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        for( auto i : nums)
+            sum += i;
+        return (((n)*(n+1))/2) - sum;
+    }
+};
