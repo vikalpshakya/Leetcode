badbd2a8a4c91511978950503efb01d387fb85d4
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-09-24 10:05:53 +0530

    Added file

--- C++/338. Counting Bits.cpp
+++ C++/338. Counting Bits.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-// TC: O(n) SC O(n)
+// ---------Appproach 1 -------//
+
+// TC:  O(n * log(n)) SC O(1) [since returntype of function is vector so its ouput vector wouldn't be considered in space count]
+
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i=0;i<=n;i++){
@@ -13,5 +16,21 @@ public:
             ans.push_back(count);
         }
         return ans;
+    }
+//     -------APPROACH 2------------    //
+
+//     Time complexity: O(n), Space complexity: O(1)
+        vector<int> dp(n+1);
+
+        if(n == 0) return dp;
+        dp[0] = 0;
+        for(int i = 1; i < n+1; i++){
+            if(i%2 != 0){
+                dp[i] = dp[i/2] + 1;
+            }
+            else 
+                dp[i] = dp[i/2];
+        }
+        return dp;
     }
 };
