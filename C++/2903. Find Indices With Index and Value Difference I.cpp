8f5919d2155396c73043cc9c0ecc70837b4ec41b
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-10-15 23:56:51 +0530

    Added file

---
+++ C++/2903. Find Indices With Index and Value Difference I.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+// Time complexity: O(n^2) Space complexity: O(1) 
+
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        for(int i =0 ; i< nums.size(); i++){
+            for(int j = 0; j < nums.size(); j++){
+                if(abs(i - j) >= indexDifference && abs(nums[i] - nums[j]) >= valueDifference){
+                    return {i,j};
+                }
+            }
+        }
+        return {-1,-1};
+    }
+};
