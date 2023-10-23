4d9a5cf13bde3fe7a7f45bd959085904d8b0fba9
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-10-23 19:41:56 +0530

    Create 342. Power of Four.cpp

---
+++ C++/342. Power of Four.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    bool solve_rec(int n){
+        if(n == 1) return true;
+        if(n%4 != 0 || n == 0) return false;
+        return solve_rec(n/4);
+    }
+    bool solve_itr(int n){
+        if(n==1||n==4) return true;
+        
+        if(n%4!=0||n==0) return false;
+        
+        int ans=1;
+        while(n%4==0 && n!=4){
+            if(n%4==0){
+                n=n/4;
+                ans=n;
+                if(n==0) break; 
+            } 
+        }
+        if(ans%4==0) return true;
+        
+        return false;   
+    } 
+
+public:
+    bool isPowerOfFour(int n) {
+        // return solve_rec(n);
+        return solve_itr(n);
+    }
+};
