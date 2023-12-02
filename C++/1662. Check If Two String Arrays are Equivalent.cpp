69b30fe59d381854bed881e2030416843a7028f1
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-12-02 18:45:47 +0530

    Added file

---
+++ C++/1662. Check If Two String Arrays are Equivalent.cpp
@@ -0,0 +1,11 @@
+class Solution {
+// TC: O(max(m,n)
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1 = "";
+        string s2 = "";
+        for(auto i : word1) s1+=i;
+        for(auto i : word2) s2+=i;
+        return s1 == s2;
+    }
+};
