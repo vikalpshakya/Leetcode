c63c72eedfc482fcd41ae93cf9e8165eeb08e684
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-12-10 14:41:06 +0530

    Added file

--- C++/130. Surrounded Regions.cpp
+++ C++/130. Surrounded Regions.cpp
@@ -13,8 +13,7 @@ private:
         return false;
     }
 
-public:
-    void solve(vector<vector<char>>& board) {
+    void bfs(vector<vector<char>>& board){
         int n = board.size();
         int m = board[0].size();
 
@@ -54,11 +53,45 @@ public:
             }
         }
     }
+
+    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, int row, int col, int delRow[], int delCol[]){
+        int n = board.size();
+        int m = board[0].size();
+        visited[row][col] = true;
+        for(int i = 0; i < 4; i++){
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+            if(isValid(nRow,nCol,n,m) && !visited[nRow][nCol] && board[nRow][nCol] == 'O'){
+                visited[nRow][nCol] = true;
+                dfs(board,visited,nRow,nCol,delRow,delCol);
+            }
+        }
+    }
+
+
+public:
+    void solve(vector<vector<char>>& board) {
+        // bfs(board);
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<bool>>visited(n,vector<bool>(m,false));
+        
+        int delRow[] = {-1, 0, +1, 0};
+        int delCol[] = {0, -1, 0, +1};
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(isBoundary(i,j,n,m) && board[i][j] == 'O'){
+                    dfs(board, visited, i, j, delCol, delRow);
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    if(!visited[i][j] && board[i][j] == 'O')
+                        board[i][j] = 'X';
+                }
+            }
+        }
 };
-
-
-
-
-
-
 
